1726e5769ddc2fa495dfb04699e98185c9fd2130
Aryan <aryandineshsingh1@gmail.com>
2026-06-11 23:48:13 +0530

    Button component completed

--- src/App.tsx
+++ src/App.tsx
@@ -10,17 +10,19 @@ export default function App() {
 
 function Header() {
   return (
-    <header className="flex items-center justify-center">
+    <header className="flex items-center justify-between">
+      {/* left side */}
       <div className="flex flex-col gap-1">
         <h1 className="text-3xl font-bold">Tracker</h1>
         <span className="text-zinc-400 text-sm">1/1</span>
       </div>
-
-      <div className="flex flex-col gap-1">
+ 
+      {/* right side */}
+      <div className="flex flex-col gap-1 items-end">
         <span className="text-zinc-400 text-sm">May 9 - May 14</span>
         <div className="flex items-center gap-3">
-          <Button text="Prev"></Button>
-          <Button text="Next"></Button>
+          <Button >Prev</Button>
+          <Button >Next</Button>
         </div>
       </div>
     </header>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,11 @@
-export function Button() {
+import type { ReactNode } from "react";
+
+type ButtonProps = {
+    children : ReactNode;
+}
+
+export function Button({children}: ButtonProps) {
     return (
-        <button>{}</button>
+        <button className="bg-violet-600 hover:bg-violet-500 transition-colors rounded px-2 py-1 disabled:opacity-30 disabled:cursor-not-allowed">{children}</button>
     )
 }
